a01bd86b293825cbdb440b864048b6e4302558d3
fentyhall <fentymhall@gmail.com>
2021-02-24 15:47:51 -0800

    adding addEventListener on hard-coded questions

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const playBtn = document.getElementById('play');
+const bodyContainer = document.querySelector('.container');
 
 // Welcome user to press the Play Button
 playBtn.addEventListener('click', (event) => {
     startGame();
 })
 
-let currentQuestion = {};
-let acceptingAnswer = true;
+let currentQues = {};
+let acceptAnswer = false;
 let score = 0;
 let questionCount = 0;
 let availableQuestions = [];
 
+// hard-coded questions for now
 let questions = [
     {
         question: 'Which company was established on April 1st, 1976 by Steve Jobs, Steve Wozniak and Ronald Wayne?',
@@ -39,50 +41,86 @@ let questions = [
 ];
 
 startGame = () => {
-    console.log('In the Game')
+    // console.log('In the Game')
     playBtn.remove();
 
     questionCount = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions); // tbd
+    // console.log(availableQuestions);
     newQuestion();
 }
 
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-
 newQuestion = () => {
-    if (availableQuestions.length === 0 || questionCount >= 10) {
+    if (availableQuestions.length === 0 || questionCount >= 3) {
         return endGame();
     }
+
     questionCount++;
+
+    // Initiate random question selections
+    let randomIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQues = availableQuestions[randomIndex];
     
     // Create <div class="container">
-    const divContainer = document.createElement('div');
-    divContainer.className = 'container';
+    let divContainer = document.createElement('div');
+    divContainer.className = 'new-card';
     divContainer.innerHTML = `
         <div id="game" class="justify-center flex-column">
-            <h2 id="question">What is the answer to this question?</h2>
+            <h2 id="question">${currentQues.question}</h2>
             <div class="choice-container">
                 <p class="choice-prefix">A</p>
-                <p class="choice-text" data-number="1">Choice 1</p>
+                <p class="choice-text" data-number="1">${currentQues.choice1}</p>
             </div>
             <div class="choice-container">
                 <p class="choice-prefix">B</p>
-                <p class="choice-text" data-number="2">Choice 2</p>
+                <p class="choice-text" data-number="2">${currentQues.choice2}</p>
             </div>
             <div class="choice-container">
                 <p class="choice-prefix">C</p>
-                <p class="choice-text" data-number="3">Choice 3</p>
+                <p class="choice-text" data-number="3">${currentQues.choice3}</p>
             </div>
             <div class="choice-container">
                 <p class="choice-prefix">D</p>
-                <p class="choice-text" data-number="4">Choice 4</p>
+                <p class="choice-text" data-number="4">${currentQues.choice4}</p>
             </div>
         </div>
     `;
+
+    bodyContainer.append(divContainer);
+    
+    // Remove the question that is currently shown from the Questions Array
+    availableQuestions.splice(randomIndex, 1);
+
+    // Allow the user to accept an answer
+    acceptAnswer = true;
+    checkAnswer();
 }
 
+// Accept User Selection and Check Answer
+checkAnswer = () => {
+    const selections = Array.from(document.getElementsByClassName('choice-text'));
+    
+    selections.forEach((selection) => {
+        selection.addEventListener('click', (event) => {
+            // If we are not ready accepting answer
+            if (!acceptAnswer) return;
+
+            // The user can only click/answer once
+            acceptAnswer = false;
+
+            const userAnswer = event.target.innerText;
+            console.log(userAnswer);
+
+            let oldDiv = document.querySelector(".new-card");
+            oldDiv.remove();
+            
+            newQuestion();
+        })
+    })
+}
+
+// Pending
 endGame = () => {
-    return "Game Over."
+    console.log("Game Over.");
 }
